516ab15edcdaa8055f7f96cb91f00d5dd1587ef7
Stanislaw Horna <stanislawhorna@outlook.com>
2024-12-23 21:08:45 +0000

    add test for currencies

---
+++ tests/test_Currencies.py
@@ -0,0 +1,89 @@
+import src.Service as Service
+from currency.currency_type_pb2 import CurrencyType
+from currency.currency_pb2 import CurrencyDetails, CurrencyTypeMsg, CurrencyList
+
+
+def test_get_currency_type_usd_upper_success():
+    s = Service.Currency()
+
+    currency_type: CurrencyTypeMsg = s.GetCurrencyType(
+        CurrencyDetails(currency_name="USD"),
+        None,
+    )
+    assert currency_type.type == CurrencyType.CURRENCY_TYPE_FIAT
+
+
+def test_get_currency_type_usd_lower_success():
+    s = Service.Currency()
+
+    currency_type: CurrencyTypeMsg = s.GetCurrencyType(
+        CurrencyDetails(currency_name="usd"),
+        None,
+    )
+    assert currency_type.type == CurrencyType.CURRENCY_TYPE_FIAT
+
+
+def test_get_currency_type_failure():
+    s = Service.Currency()
+
+    currency_type: CurrencyTypeMsg = s.GetCurrencyType(
+        CurrencyDetails(currency_name="fwqewfe"),
+        None,
+    )
+    assert currency_type.type == CurrencyType.CURRENCY_TYPE_NOT_SUPPORTED
+
+
+def test_get_currency_type_dai_upper_success():
+    s = Service.Currency()
+
+    currency_type: CurrencyTypeMsg = s.GetCurrencyType(
+        CurrencyDetails(currency_name="DAI"),
+        None,
+    )
+    assert currency_type.type == CurrencyType.CURRENCY_TYPE_CRYPTO
+
+
+def test_get_currency_type_dai_lower_success():
+    s = Service.Currency()
+
+    currency_type: CurrencyTypeMsg = s.GetCurrencyType(
+        CurrencyDetails(currency_name="dai"),
+        None,
+    )
+    assert currency_type.type == CurrencyType.CURRENCY_TYPE_CRYPTO
+
+
+def test_get_supported_currencies_fiat_success():
+    s = Service.Currency()
+
+    currency_list: CurrencyList = s.GetSupportedCurrencies(
+        CurrencyTypeMsg(type=CurrencyType.CURRENCY_TYPE_FIAT),
+        None,
+    )
+    assert len(currency_list.currencies) == 46
+    assert "usd" in [c.currency_name for c in currency_list.currencies]
+    assert "eur" in [c.currency_name for c in currency_list.currencies]
+    assert "pln" in [c.currency_name for c in currency_list.currencies]
+
+
+def test_get_supported_currencies_crypto_success():
+    s = Service.Currency()
+
+    currency_list: CurrencyList = s.GetSupportedCurrencies(
+        CurrencyTypeMsg(type=CurrencyType.CURRENCY_TYPE_CRYPTO),
+        None,
+    )
+    assert len(currency_list.currencies) == 50
+    assert "bitcoin" in [c.currency_name for c in currency_list.currencies]
+    assert "ethereum" in [c.currency_name for c in currency_list.currencies]
+    assert "tether" in [c.currency_name for c in currency_list.currencies]
+
+
+def test_get_supported_currencies_failure():
+    s = Service.Currency()
+
+    currency_list: CurrencyList = s.GetSupportedCurrencies(
+        CurrencyTypeMsg(type=12),
+        None,
+    )
+    assert len(currency_list.currencies) == 0
